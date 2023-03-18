bf51c637b1565a352061e1135132b434de9bc32e
sano84 <sano84@yahoo.com>
2023-03-18 15:13:30 -0400

    new update

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.story-btn').forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        // console.log('clicked');
+        btn.classList.toggle('change')
+        btn.nextElementSibling.classList.toggle('change')
+    })
+})
